f1a42d7dfdee5be24711a72a257a905063eb26ce
Jonathan Rosario <archidynamics@yahoo.com>
2014-03-18 14:01:11 -0400

    Lab 7 revision 2:00pm

---
+++ Rosario_Jonathan_Lab_7.js
@@ -0,0 +1,11 @@
+//alert("JavaScript works!");
+
+var myMovie = function(title, year, studio, director) {
+	
+	this.title = title;
+	this.year = year;
+	this.studio = studio;
+	this.director = director;
+	this.currentSpeed = currentSpeed;
+	this.accelerate = function(increase) {
+		
---
+++ Rosario_Jonathan_Lab_7_json.js
@@ -0,0 +1,26 @@
+//alert("JavaScript works!");
+
+var jsonData = {
+	
+	"myMovie":
+	[
+	
+		{
+			"title": "Psycho",
+			"year": 1960,
+			"studio": "Paramount Pictures",
+			"director": "Alfred Hitchcock",
+			
+		},
+		{
+			
+			"title": "Vertigo",
+			"year": 1958,
+			"studio": "Paramount Pictures",
+			"director": "Alfred Hitchcock",
+					
+		}
+	
+	]
+
+};
